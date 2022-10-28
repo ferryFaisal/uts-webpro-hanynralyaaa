8ee2bde7039cd44ea49ad11b87a31bfde0717bbe
hanynralyaaa <hani.nuralya25@gmail.com>
2022-10-28 16:07:38 +0700

    Update create_table.php

--- create_table.php
+++ create_table.php
@@ -1,13 +1,15 @@
 <?php
 require "database.php";
 
-try {
-    $conn = new PDO("mysql:host=$host;dbname=$db_name", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  
-    // sql to create table
-    $sql = "CREATE TABLE IF NOT EXISTS products (
+// Create connection
+$conn = new mysqli($host, $username, $password, $db_name);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// sql to create table
+$sql = "CREATE TABLE IF NOT EXISTS products (
               id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
               name varchar(128) NOT NULL,
               description text NOT NULL,
@@ -16,13 +18,12 @@ try {
               created datetime NOT NULL,
               modified timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
             ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9";
-    
-    // use exec() because no results are returned
-    $conn->exec($sql);
-    echo "Table products created successfully";
-  } catch(PDOException $e) {
-    echo $sql . "<br>" . $e->getMessage();
-  }
-  
-$conn = null;
-?>
+
+// use exec() because no results are returned
+if ($conn->query($sql) === TRUE) {
+  echo "Table products created successfully";
+} else {
+  echo "Error creating table: " . $conn->error;
+}
+
+$conn->close();
